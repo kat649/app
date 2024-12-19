3f4ef23ac0ec0eff018a58fb4c0d9c9b357d12d1
kat649 <k9457365@gmail.com>
2024-12-19 09:53:23 +0500

    Initial commit

---
+++ app/main-view-model.ts
@@ -0,0 +1,8 @@
+import { Observable } from '@nativescript/core';
+import { CalculatorModel } from './models/calculator';
+
+export class HelloWorldModel extends CalculatorModel {
+  constructor() {
+    super();
+  }
+}
